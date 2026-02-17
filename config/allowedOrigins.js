9b5a89ab37b16b41b861ab6a2c1d38550cd4dc7d
peter514 <peterkgachora1@gmail.com>
2026-02-17 11:24:22 +0300

    updated allowed origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -6,6 +6,8 @@ const allowedOrigins = [
   "http://192.168.100.45:3000",
   "https://admin.drfullz.com",
   "https://www.admin.drfullz.com",
+  "https://www.fullducks.com",
+  "https://fullducks.com",
 ];
 
 module.exports = allowedOrigins;
