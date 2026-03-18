005e483ba042bf8585980c954c80b0b60c60bc70
anto254 <jenga.anto@gmail.com>
2026-03-18 12:43:03 +0300

    fixes

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -132,7 +132,14 @@ const removeFromCart = async (req, res) => {
 };
 // function to generate a batch number for a checkout
 function generateBatchNumber() {
-  return `BATCH-${Date.now()}`;
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  return `BATCH-${year}${month}${day}${hours}${minutes}${seconds}`;
 }
 
 const checkoutItems = async (req, res) => {
