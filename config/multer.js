faf38c4a2f1d3cc0d3784f63ca9adfba5d587982
anto254 <jenga.anto@gmail.com>
2026-04-06 22:52:48 +0300

    upgraded csv =size limit from 10 to 200mb

--- config/multer.js
+++ config/multer.js
@@ -30,7 +30,7 @@ const storage = multer.diskStorage({
 const upload = multer({
   storage: storage,
   limits: {
-    fileSize: 10 * 1024 * 1024 // 10MB limit
+    fileSize: 200 * 1024 * 1024 // 200MB limit
   },
   fileFilter: (req, file, cb) => {
     // Check file type
